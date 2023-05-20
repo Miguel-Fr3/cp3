30a247a880b5314314a56d5914814818d8033ed2
Albertoseiji <alberto.assatoalberto@gmail.com>
2023-05-20 00:17:11 -0300

    finalizando o botão

--- js/cp3.js
+++ js/cp3.js
@@ -45,11 +45,96 @@ function validateFields() {
 
 const checkbox = document.querySelector('.checkbox');
 
-
 checkbox.addEventListener('change', function() {
 
-  const body = document.body;
-
-  body.classList.toggle('form-check-escuro');
-
-});
+    const body = document.body;
+  
+    body.classList.toggle('form-check-escuro');
+  
+  });
+  
+  function applyLightModeStyles() {
+    const form = document.querySelector('form-check');
+    form.classList.remove('form-check-escuro');
+  
+    const title = document.querySelector('h1');
+    title.classList.remove('dark');
+  
+    const formElements = form.querySelectorAll('label, input[type="text"], input[type="email"], input[type="password"], button[type="submit"], .error');
+    formElements.forEach(element => {
+      element.classList.remove('form-check-escuro');
+    });
+  }
+  
+  function applyDarkModeStyles() {
+    const form = document.querySelector('form-check');
+    form.classList.add('form-check-escuro');
+  
+    const title = document.querySelector('h1');
+    title.classList.add('form-check-escuro');
+  
+    const formElements = form.querySelectorAll('label, input[type="text"], input[type="email"], input[type="password"], button[type="submit"], .error');
+    formElements.forEach(element => {
+      element.classList.add('form-check-escuro');
+    });
+  }
+  
+  const form = document.getElementById("registration");
+  const username = document.getElementById("username");
+  const email = document.getElementById("email");
+  const password = document.getElementById("password");
+  const passwordConfirmation = document.getElementById("password-confirmation");
+  
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+  
+    checkInputs();
+  });
+  
+  document.getElementById('cadastroForm').addEventListener('submit', function(event) {
+      event.preventDefault(); // Impede o envio do formulário para testar a validação
+  
+      validateFields();
+  });
+  
+  function validateFields() {
+      const firstName = document.getElementById('firstName').value;
+      const lastName = document.getElementById('lastName').value;
+      const email = document.getElementById('email').value;
+      const password = document.getElementById('password').value;
+      const confirmPassword = document.getElementById('confirmPassword').value;
+  
+      // Validação do Primeiro Nome
+      if (firstName.length < 5) {
+          alert('O primeiro nome deve ter no mínimo 5 caracteres.');
+          return;
+      }
+  
+      // Validação do Segundo Nome
+      if (lastName.length < 5) {
+          alert('O segundo nome deve ter no mínimo 5 caracteres.');
+          return;
+      }
+  
+      // Validação do Email
+      if (email.length < 5 || email.indexOf('@') === -1) {
+          alert('O email deve ter no mínimo 5 caracteres e conter o caractere "@"');
+          return;
+      }
+  
+      // Validação da Senha
+      if (password.length < 6 || password.length > 8) {
+          alert('A senha deve ter entre 6 e 8 caracteres.');
+          return;
+      }
+  
+      // Validação da Confirmação de Senha
+      if (password !== confirmPassword) {
+          alert('A confirmação de senha deve ser igual à senha digitada.');
+          return;
+      }
+  
+      // Todas as validações passaram, então o formulário pode ser enviado
+      alert('Formulário enviado com sucesso!');
+  
+    }
