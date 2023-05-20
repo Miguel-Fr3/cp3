c5dfca8abe08a16f85e63b750b01fd790239c322
Albertoseiji <alberto.assatoalberto@gmail.com>
2023-05-20 00:11:01 -0300

    ajustando validação

--- js/cp3.js
+++ js/cp3.js
@@ -11,30 +11,29 @@ function validateFields() {
         return;
     }
 
-    // Validação do Segundo Nome
+
     if (lastName.length < 5) {
         alert('O segundo nome deve ter no mínimo 5 caracteres.');
         return;
     }
 
-    // Validação do Email
+
     if (email.length < 5  || email.indexOf('@') === -1) {
         alert('O email deve ter no mínimo 5 caracteres e conter o caractere "@"');
         return;
     }
 
-    // Validação da Senha
+
     if (password.length < 6  || password.length > 8) {
         alert('A senha deve ter entre 6 e 8 caracteres.');
         return;
     }
 
-    // Validação da Confirmação de Senha
     if (password !== confirmPassword) {
         alert('A confirmação de senha deve ser igual à senha digitada.');
         return;
     }
 
-    // Todas as validações passaram, então o formulário pode ser enviado
+
     alert('Formulário enviado com sucesso!');
 }
